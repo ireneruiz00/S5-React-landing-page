b68a5bb2c7f8d826d510065a54600e19bc8fa758
ireneruiz00 <ruizsirene2@gmail.com>
2025-06-03 11:30:05 +0200

    Expansion of the FAQs as an accordion using useState

--- src/components/FAQs/FAQs.jsx
+++ src/components/FAQs/FAQs.jsx
@@ -2,18 +2,36 @@ import React from 'react'
 import "./FAQs.css"
 import iconArrow from "../../assets/Imagenes/icon-arrow.svg"
 import Buttons from '../Buttons/Buttons'
+import "../Buttons/Buttons.css"
+import { useState } from 'react'
+import { FaqData } from './FaqData'
 
 function FAQs() {
+  const [itemExpanded, setItemExpanded] = useState(null)
+  const toggleItem = (index) => {
+    setItemExpanded(prev => (prev === index ? null : index));
+  }
   return (
     <section className="FAQs">
-        <div className="text-center text-margin">
+        <div className="text-margin">
+          <div className="text-center">
             <h2>Frequently asked questions</h2>
             <p>Here are some of our FAQs. If you have any other questions you'd like answered please feel free to email us.</p>
+          </div>
             <ul className="flex">
-                <li>What is Bookmark? <img src={iconArrow} alt="icon-arrow"/></li> 
-                <li>How can I request a new browser? <img src={iconArrow} alt="icon-arrow"/></li>
-                <li>Is there a mobile app? <img src={iconArrow} alt="icon-arrow"/></li>
-                <li>What about other Chromium browsers? <img src={iconArrow} alt="icon-arrow"/></li>
+              {FaqData.map((item, index) =>
+                <li key={index} className={`faq-item ${itemExpanded === index ? 'active' : ''}`}>
+                  <div className="faq-question" onClick={() => toggleItem(index)}>
+                    {item.question}
+                    <button className='btn-arrow'><img src={iconArrow} alt="icon-arrow" className={itemExpanded === index ? 'rotated' : ''}/></button>
+                  </div>
+                  {itemExpanded === index && (
+                  <div className="faq-answer">
+                    {item.answer}
+                  </div>
+                )}
+                </li>  
+              )}
             </ul>
             <Buttons button_class = {'btn-azul-centered'} button_content = {'More info'}/>
         </div>
